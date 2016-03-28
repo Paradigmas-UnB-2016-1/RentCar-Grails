415a80a1ea6fb6ccc7180dcacde82d92cdcd77bc
Eduardo Moreira <moreira.edu@hotmail.com>
2016-03-28 13:18:50 -0300

    Criando intervalo para valores de cor do carro

--- grails-app/domain/rentcar/grails/Carro.groovy
+++ grails-app/domain/rentcar/grails/Carro.groovy
@@ -16,7 +16,8 @@ class Carro {
     	nome nullable:false, blank:false
     	categoria inList: ["Economico","Compacto","Esportivo","Luxo","Pick up","SUV","Grande","Medio"]
     	capacidadeDePessoas min:1, max:8
-    	corDoVeiculo nullable:false, blank:false
+    	corDoVeiculo nullable:false, blank:false, inList: ["Branco","Preto","Prata","Cinza","Azul","Vermelho",
+    														"Amarelo","Laranja","Verde"]
     	numeroDoChassi nullable:false, unique:true
     	valorDoDia nullable:false
     }
