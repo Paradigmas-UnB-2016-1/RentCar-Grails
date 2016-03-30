4daa2240316c1107b13217084577dd1c862ea0ea
Eduardo_Nunes <eduardonunes2525@gmail.com>
2016-03-29 23:21:21 -0300

    Add constraints na classe Aluguel

--- grails-app/domain/rentcar/grails/Aluguel.groovy
+++ grails-app/domain/rentcar/grails/Aluguel.groovy
@@ -5,10 +5,10 @@ class Aluguel {
     Date dataInicio
 	Date dataFim
 
-	static belongsTo = [carro: Carro,
-						cliente: Cliente,
-						funcionario: Funcionario]
+	static belongsTo = [carro: Carro, cliente: Cliente,funcionario: Funcionario]
 
     static constraints = {
+    	dataInicio nullable:false
+    	dataFim nullable:false
     }
 }
