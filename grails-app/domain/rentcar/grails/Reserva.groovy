694a4ad357d524bbc6b535ebd8fc9077525152bd
Eduardo_Nunes <eduardonunes2525@gmail.com>
2016-03-29 23:25:03 -0300

    Add constraints na classe Reserva

--- grails-app/domain/rentcar/grails/Reserva.groovy
+++ grails-app/domain/rentcar/grails/Reserva.groovy
@@ -5,9 +5,10 @@ class Reserva {
 	Date dataInicio
 	Date dataFim
 
-	static belongsTo = [carro: Carro,
-						cliente: Cliente]
+	static belongsTo = [carro: Carro, cliente: Cliente]
 
     static constraints = {
+    	dataInicio nullable:false
+    	dataFim nullable:false
     }
 }
